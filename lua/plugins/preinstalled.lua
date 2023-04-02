8df9831c0bda7a6e9bfa239ffaaae66c2e00f75a
Rythmyr@gmail.com <rythmyr@gmail.com>
2023-04-02 17:49:41 -0500

    lspconfig rust use rust-tools.nvim

--- lua/plugins/preinstalled.lua
+++ lua/plugins/preinstalled.lua
@@ -23,6 +23,24 @@ return {
     },
   },
   {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "simrat39/rust-tools.nvim",
+    },
+    opts = {
+      servers = {
+        rust_analyzer = {},
+      },
+      setup = {
+        rust_analyzer = function(_, opts)
+          require("rust-tools").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+
+  {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
