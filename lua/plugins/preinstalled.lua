aa5d6587a70639583879a1b670cf1bb4c011bc1d
Rythmyr@gmail.com <rythmyr@gmail.com>
2023-04-02 17:48:28 -0500

    neo-tree show hidden items in tree

--- lua/plugins/preinstalled.lua
+++ lua/plugins/preinstalled.lua
@@ -22,4 +22,16 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
 }
